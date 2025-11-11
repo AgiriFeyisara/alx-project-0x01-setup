86f93255cd025ce497e78e0e6f940bfa31517eb6
Feyisara Agiri <mohrenny8@gmail.com>
2025-11-11 18:50:16 +0100

    Reused the header components in the page route

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import PostCard from "@/components/common/PostCard";
+import Header from "@/components/layout/Header";
 
 const PostsPage: React.FC = () => {
   return (
     <div className="max-w-4xl mx-auto p-8">
       <h1 className="text-3xl font-bold mb-6">Posts</h1>
+      <Header/>
       <PostCard title="Post 1" description="This is the description of Post 1." />
       <PostCard title="Post 2" description="This is the description of Post 2." />
     </div>
--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Header from "@/components/layout/Header";
 
 const UsersPage: React.FC = () => {
   return (
     <div className="max-w-4xl mx-auto p-8">
       <h1 className="text-3xl font-bold mb-6">Users</h1>
+      <Header/>
       <p>List of users will appear here.</p>
     </div>
   );
