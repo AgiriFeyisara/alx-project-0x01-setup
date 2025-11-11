dd1a9f8f1632f522056820348530b1284cd2a4b3
Feyisara Agiri <mohrenny8@gmail.com>
2025-11-11 19:33:17 +0100

    Corrected some errors

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -64,5 +64,5 @@ export interface UserData{
 export interface UserModalProps {
   isOpen: boolean;
   onClose: () => void;
-  onSave: (user: UserData) => void;
+  onSubmit: (post: UserData) => void; 
 }
